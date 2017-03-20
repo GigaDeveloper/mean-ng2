18680fa7801075c0da25886292d273ef74349019
Giga Songulashvili <giga@unipay.ge>
2017-03-20 19:10:14 +0400

    Add menu and routes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./components/home/home.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {LoginComponent} from "./components/login/login.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
